4fee8741eceb9b56fecbfcbf6c238b49ae69a88c
Tiago Loureiro <19423709+tiagoicp@users.noreply.github.com>
2026-06-13 00:05:27 +0100

    added dependabot and changed format/linter (#1)
    * added dependabot and changed format/linter
    
    * reverted on my change of the trailing comma
    
    * resolved comments

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    extends: [...tseslint.configs.recommended],
+    files: ["src/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_"
+        }
+      ]
+    }
+  },
+  {
+    ignores: ["env.d.ts", "node_modules/"]
+  }
+);
